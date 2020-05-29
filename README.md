9a7fe79eddb70a0f4f28a6f8f6e0d9932deb96d3
tjburrows <37048572+tjburrows@users.noreply.github.com>
2020-05-29 10:54:26 -0400

    Add docstring, out-of-memory features

--- README.md
+++ README.md
@@ -4,16 +4,78 @@ This is a port of the Towne et al. SPOD Matlab function to Python 3.
 ## Current Status
 Basic spod computation works, and example results match well.  Results do not match exactly in part because of different (but still valid) eigenvectors from Numpy.
 
-## To Do
- - Update this Readme
- - Add implementation of out-of-memory computation (savefft, deletefft, savedir, savefreqs, loadfft)
- - improve commenting, formatting of code
- 
- ### Requirements
+### Requirements
  Install the below requirements with `pip install requirements.txt`
  - spod: numpy, scipy
  - examples: matplotlib, h5py
- 
+
+### Usage
+spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isreal=None, nt=None, conflvl=None, normvar=False, debug=0, lowmem=False, savefile=None)
+    
+    Parameters
+    ----------
+    x : array or function object
+        Data array whose first dimension is time, or function that retrieves
+        one snapshot at a time like x(i).  x(i), like x, can have any dimension. 
+        If x is a function, it is recommended to specify the total number of 
+        snaphots in nt (see below). If not specified, nt defaults to 10000. 
+        isreal should be specified if a two-sided spectrum is desired even 
+        though the data is real-valued, or if the data is initially real-valued,
+        but complex-valued-valued for later snaphots.
+    window : vector, int, or string, optional
+        A temporal window. If WINDOW is a vector, X
+        is divided into segments of the same length as WINDOW. Each segment is
+        then weighted (pointwise multiplied) by WINDOW. If WINDOW is a scalar,
+        a Hamming window of length WINDOW is used. If WINDOW is none or 'hamming',
+        a Hamming window is used.
+    weight : array, optional
+        A spatial inner product weight.  SPOD modes are optimally ranked and 
+        orthogonal at each frequency. WEIGHT must have the same spatial 
+        dimensions as x.
+    noverlap : int, optional
+        Number of snaptions to overlap consecutive blocks.  noverlap defaults
+        to 50% of the length of WINDOW if not specified.
+    dt : float, optional
+        Time step between consecutive snapshots to determine a physical 
+        frequency F.  dt defaults to 1 if not specified.
+    mean : array or string, optional
+        A mean that is subtracted from each snapshot.  If 'blockwise', the mean
+        of each block is subtracted from itself.  If x is a function the mean
+        provided is a temporal mean.
+    isreal : bool, optional
+        Describes if x data is real.
+    nt : int, optional
+        Number of snapshots.  If x is an array, this is determined from x
+        dimensions.  If x is a function, this defaults to 10000 if not specified.
+    conflvl : bool or float, optional
+        Calculate and return confidence interval levels of L (Lc).  If True,
+        the lower and upper 95% confidence levels of the j-th
+        most energetic SPOD mode at the i-th frequency are returned in
+        Lc(i,j,1) and Lc(i,j,2), respectively.  If a float between 0 and 1, 
+        the conflvl*100% confidence interval of L is returned. A 
+        chi-squared distribution is used, i.e. we assume a standard normal 
+        distribution of the SPOD eigenvalues.  Defaults to None/False.
+    normvar : bool, optional
+        Normalize each block by pointwise variance.  Defaults to false.
+    debug : {0, 1, 2}, optional
+        Verbosity of output.  0 hides output.  1 shows some output, 2 shows all output.
+        Defaults to 0.
+    lowmem : bool, optional
+        Specifies whether to use low-memory mode.  If True, this stores the FFT blocks in a
+        temporary file on disk, and also stores all returned quantities on disk, returning
+        a h5py handle for the file. Default is False, keeping everything in memory.  If
+        lowmem is True, savefile must be specified for the returned data.
+    savefile : string, optional
+        Filename to which to save the results in HDF5 format.  If lowmem is True,
+        a handle for this file is returned.  If False or None, the in-memory results 
+        are returned in a dictionary.  Defaults to None/False.
+    
+    Returns
+    -------
+    output
+        A dictionary containing SPOD modes (P), modal energy spectra (L), and frequency
+        vector (f).  If conflvl is specified, confidence interval is returned (Lc).  If
+        lowmem is True, a file object is returned which points to the on-disk HDF5 dataset.
 
 ## Original Matlab Documentation
 SPOD() is a Matlab implementation of the frequency domain form of proper orthogonal decomposition (POD, also known as principle component analysis or Karhunen-Loève decomposition) called spectral proper orthogonal decomposition (SPOD). SPOD is derived from a space-time POD problem for stationary flows [[1](https://arxiv.org/abs/1708.04393),2] and leads to modes that each oscillate at a single frequency. SPOD modes represent dynamic structures that optimally account for the statistical variability of stationary random processes.
--- spod.py
+++ spod.py
@@ -3,13 +3,12 @@ from types import FunctionType
 from warnings import warn
 from scipy.special import gammaincinv
 import scipy
+import os.path
+import os
 
 # Hamming window
 def hammwin(N):
     return 0.54 - 0.46 * np.cos(2 * np.pi * np.arange(N) / (N - 1))
-
-def nextpow2(n):
-    return np.ceil(np.log2(np.abs(n)))
 
 # print output based on verbosity level
 def printer(string, level):
@@ -20,6 +19,7 @@ def printer(string, level):
 # parse spod arguments
 def spod_parser(nt, nx, isrealx, window, weight, noverlap, conflvl):
     
+    # window size and type
     if window == 'hamming' or window is None:
         nDFT = 2 ** np.floor(np.log2(nt / 10.0)).astype(int)
         window = hammwin(nDFT)
@@ -83,12 +83,90 @@ def spod_parser(nt, nx, isrealx, window, weight, noverlap, conflvl):
     
     return (window, weight, noverlap, nDFT, nBlks, conflvl)
 
-def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isreal=None, nt = None, conflvl=None, normvar=False, debug=0):
+def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isreal=None, nt=None, conflvl=None, normvar=False, debug=0, lowmem=False, savefile=None):
+    """"
+    Spectral proper orthogonal decomposition
     
+    Parameters
+    ----------
+    x : array or function object
+        Data array whose first dimension is time, or function that retrieves
+        one snapshot at a time like x(i).  x(i), like x, can have any dimension. 
+        If x is a function, it is recommended to specify the total number of 
+        snaphots in nt (see below). If not specified, nt defaults to 10000. 
+        isreal should be specified if a two-sided spectrum is desired even 
+        though the data is real-valued, or if the data is initially real-valued,
+        but complex-valued-valued for later snaphots.
+    window : vector, int, or string, optional
+        A temporal window. If WINDOW is a vector, X
+        is divided into segments of the same length as WINDOW. Each segment is
+        then weighted (pointwise multiplied) by WINDOW. If WINDOW is a scalar,
+        a Hamming window of length WINDOW is used. If WINDOW is none or 'hamming',
+        a Hamming window is used.
+    weight : array, optional
+        A spatial inner product weight.  SPOD modes are optimally ranked and 
+        orthogonal at each frequency. WEIGHT must have the same spatial 
+        dimensions as x.
+    noverlap : int, optional
+        Number of snaptions to overlap consecutive blocks.  noverlap defaults
+        to 50% of the length of WINDOW if not specified.
+    dt : float, optional
+        Time step between consecutive snapshots to determine a physical 
+        frequency F.  dt defaults to 1 if not specified.
+    mean : array or string, optional
+        A mean that is subtracted from each snapshot.  If 'blockwise', the mean
+        of each block is subtracted from itself.  If x is a function the mean
+        provided is a temporal mean.
+    isreal : bool, optional
+        Describes if x data is real.
+    nt : int, optional
+        Number of snapshots.  If x is an array, this is determined from x
+        dimensions.  If x is a function, this defaults to 10000 if not specified.
+    conflvl : bool or float, optional
+        Calculate and return confidence interval levels of L (Lc).  If True,
+        the lower and upper 95% confidence levels of the j-th
+        most energetic SPOD mode at the i-th frequency are returned in
+        Lc(i,j,1) and Lc(i,j,2), respectively.  If a float between 0 and 1, 
+        the conflvl*100% confidence interval of L is returned. A 
+        chi-squared distribution is used, i.e. we assume a standard normal 
+        distribution of the SPOD eigenvalues.  Defaults to None/False.
+    normvar : bool, optional
+        Normalize each block by pointwise variance.  Defaults to false.
+    debug : {0, 1, 2}, optional
+        Verbosity of output.  0 hides output.  1 shows some output, 2 shows all output.
+        Defaults to 0.
+    lowmem : bool, optional
+        Specifies whether to use low-memory mode.  If True, this stores the FFT blocks in a
+        temporary file on disk, and also stores all returned quantities on disk, returning
+        a file handle. Default is False, keeping everything in memory.  If
+        lowmem is True, savefile must be specified for the returned data.  This mode requires
+        the h5py package.
+    savefile : string, optional
+        Filename to which to save the results in HDF5 format.  If lowmem is True,
+        a handle for this file is returned.  If False or None, the in-memory results 
+        are returned in a dictionary.  If file exists, it is overwritten.  Defaults to None/False.
+    
+    Returns
+    -------
+    output
+        A dictionary containing SPOD modes (P), modal energy spectra (L), and frequency
+        vector (f).  If conflvl is specified, confidence interval is returned (Lc).  If
+        lowmem is True, a file object is returned which points to the on-disk HDF5 dataset.
+    """
     # verbosity of output
     global verbosity
     verbosity = debug
     
+    # In low memory mode, a save file must be specified
+    if lowmem:
+        import tempfile
+        import h5py
+        assert savefile is not None, "savefile must be provided in lowmem mode"
+    
+    # Warn about overwriting file
+    if savefile and os.path.exists(savefile):
+        warn('%s will be overwritten' % savefile)
+        
     # Get problem dimensions
     if isinstance(x, FunctionType) or callable(x):
         xfun = True
@@ -105,10 +183,7 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
         nt = dim[0]
         nx = np.prod(dim[1:]).astype(int)
 
-    # Determine whether data is real-valued or complex-valued-valued to decide on one- or two-sided
-    # spectrum. If opts['isreal'] is not set, determine from data. If data is
-    # provided through a function handle XFUN and opts.isreal is not specified,
-    # determine complex-valuedity from first snapshot.
+    # Determine if data is real or complex
     if isreal is not None:
         isrealx = isreal
     elif not xfun:
@@ -121,6 +196,7 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
     if not xfun:
         x = np.float64(x) if isrealx else np.complex128(x)
     
+    # Parse parameters
     window,weight,noverlap,nDFT,nBlks,conflvl = spod_parser(nt, nx, isrealx, window, weight, noverlap, conflvl)
     
     # determine correction for FFT window gain
@@ -132,6 +208,7 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
     else:
         blk_mean = False
     
+    # Calculate mean
     if xfun:
         if (mean is not None) and (not blk_mean):
             x_mean = mean.flatten()
@@ -160,11 +237,31 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
             f[nDFT/2:] -= 1.0 / dt
         else:
             f[(nDFT+1)/2:] -= 1.0 / dt
+    
+    # if savefreqs is not None:
+    #     new_f = []
+    #     for freq in savefreqs:
+    #         if freq in f and freq not in new_f:
+    #             new_f.append(freq)
+    #         else:
+    #             fidx = np.argmin(np.abs(freq-f))
+    #             if f[fidx] not in new_f:
+    #                 warn('%g Hz does not exactly match.  Using %g instead.' % (freq, f[fidx]))
+    #                 new_f.append(f[fidx])
+    #     f = new_f
     nFreq = np.size(f)
+    
+    # In low memory mode, open temporary file for Q_hat.  Else, hold in memory.
+    if lowmem:
+        tempf = tempfile.TemporaryFile()
+        tempfh5 = h5py.File(tempf,'a')
+        Q_hat = tempfh5.create_dataset("Q_hat", (nFreq, nx, nBlks), dtype=np.cdouble)
+    else:
+        Q_hat = np.zeros((nFreq, nx, nBlks), dtype=np.cdouble)
+        
     
     # loop over number of blocks and generate Fourier realizations
     printer('\nCalculating temporal DFT\n------------------------------------', 1)
-    Q_hat = np.zeros((nFreq, nx, nBlks), dtype=np.cdouble)
     for iBlk in range(nBlks):
         offset = int(min(iBlk * (nDFT - noverlap) + nDFT, nt) - nDFT)
         timeIdx = np.arange(nDFT) + offset
@@ -203,12 +300,24 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
         # keep FFT blocks in memory
         Q_hat[:,:,iBlk] = Q_blk_hat
     
+    # Dimensions of P
+    pDim = [nFreq] + list(dim[1:]) + [nBlks]
+    
+    # In low memory mode, save L and P to files.  Else, hold in memory.
+    if lowmem:
+        output = h5py.File(savefile,'a')
+        if 'L' in output:
+            del output['L']
+        L = output.create_dataset("L", (nFreq, nBlks), dtype=np.double)
+        if 'P' in output:
+            del output['P']
+        P = output.create_dataset("P", pDim, dtype=np.cdouble)
+    else:
+        L = np.zeros((nFreq, nBlks),  dtype=np.double)
+        P = np.zeros(pDim, dtype=np.cdouble)
+        
     # loop over all frequencies and calculate SPOD
-    L = np.zeros((nFreq, nBlks),  dtype=np.cdouble)
     printer('\nCalculating SPOD\n------------------------------------', 1)
-   
-    # keep everything in memory
-    P   = np.zeros((nFreq,nx,nBlks), dtype=np.cdouble)
     for iFreq in range(nFreq):
         printer('frequency %d / %d (f=%g)' % (iFreq+1, nFreq, f[iFreq]), 2)
         Q_hat_f = Q_hat[iFreq, :, :]
@@ -218,19 +327,38 @@ def spod(x, window='hamming', weight=None, noverlap=None, dt=1, mean=None, isrea
         Lambda = Lambda[idx]
         Theta = Theta[:, idx]          
         Psi = np.matmul(np.matmul(Q_hat_f, Theta), np.diag(np.reciprocal(np.lib.scimath.sqrt(Lambda)) / np.sqrt(nBlks)))
-        P[iFreq,:,:] = Psi # mode
+        P[iFreq,:] = Psi.reshape(pDim[1:]) # mode
         L[iFreq, :] = np.abs(Lambda) # energy distribution
-        
-    newDim = [nFreq] + list(dim[1:]) + [nBlks]
-    P = np.reshape(P, newDim)
-    L = np.real_if_close(L)
-    P = np.real_if_close(P)
-    output = {'L':L, 'P':P, 'f':f}
     
+    # Cast to real if all modes are real
+    P = np.real_if_close(P)
+    
+    # Calculate confidence interval
     if conflvl:
-        Lc = np.zeros((nFreq, nBlks, 2), dtype=L.dtype)
+        Lc = output.create_dataset("Lc", shape=(nFreq, nBlks, 2), dtype=L.dtype) if lowmem else np.zeros((nFreq, nBlks, 2), dtype=L.dtype)
         Lc[:, :, 0] = L * nBlks / gammaincinv(nBlks, conflvl)
         Lc[:, :, 1] = L * nBlks / gammaincinv(nBlks, 1.0 - conflvl)
-        output['Lc'] = Lc
-        
+    
+    # If held in memory, create output dictionary
+    if not lowmem:
+        output = {'L':L, 'P':P, 'f':f}
+        if conflvl:
+            output['Lc'] = Lc
+    
+    # Create output save file
+    if savefile:
+        if lowmem:
+            tempfh5.close()
+            tempf.close()
+            if 'f' in output:
+                del output['f']
+            output.create_dataset('f', data=f)
+        else:
+            with h5py.File(savefile,'a') as outputfile:
+                for key,value in output.items():
+                    if key in outputfile:
+                        del outputfile[key]
+                    outputfile.create_dataset(key, data=value)
+    
+    # Return either in-memory or on-disk dictionary
     return output
